e2ef8a00a08efb95d348121d8731001c6f25afc1
Michael Wesley Sabino da Silva <micwesley@gmail.com>
2021-07-25 10:32:16 -0300

    MVC maven

---
+++ src/main/java/com/mycompany/pizzaproject/controllers/ClienteTbJpaController.java
@@ -0,0 +1,204 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pizzaproject.controllers;
+
+import com.mycompany.pizzaproject.controllers.exceptions.IllegalOrphanException;
+import com.mycompany.pizzaproject.controllers.exceptions.NonexistentEntityException;
+import com.mycompany.pizzaproject.models.ClienteTb;
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import com.mycompany.pizzaproject.models.PedidoTb;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Michael
+ */
+public class ClienteTbJpaController implements Serializable {
+
+    public ClienteTbJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(ClienteTb clienteTb) {
+        if (clienteTb.getPedidoTbCollection() == null) {
+            clienteTb.setPedidoTbCollection(new ArrayList<PedidoTb>());
+        }
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Collection<PedidoTb> attachedPedidoTbCollection = new ArrayList<PedidoTb>();
+            for (PedidoTb pedidoTbCollectionPedidoTbToAttach : clienteTb.getPedidoTbCollection()) {
+                pedidoTbCollectionPedidoTbToAttach = em.getReference(pedidoTbCollectionPedidoTbToAttach.getClass(), pedidoTbCollectionPedidoTbToAttach.getPedidoId());
+                attachedPedidoTbCollection.add(pedidoTbCollectionPedidoTbToAttach);
+            }
+            clienteTb.setPedidoTbCollection(attachedPedidoTbCollection);
+            em.persist(clienteTb);
+            for (PedidoTb pedidoTbCollectionPedidoTb : clienteTb.getPedidoTbCollection()) {
+                ClienteTb oldClienteIdOfPedidoTbCollectionPedidoTb = pedidoTbCollectionPedidoTb.getClienteId();
+                pedidoTbCollectionPedidoTb.setClienteId(clienteTb);
+                pedidoTbCollectionPedidoTb = em.merge(pedidoTbCollectionPedidoTb);
+                if (oldClienteIdOfPedidoTbCollectionPedidoTb != null) {
+                    oldClienteIdOfPedidoTbCollectionPedidoTb.getPedidoTbCollection().remove(pedidoTbCollectionPedidoTb);
+                    oldClienteIdOfPedidoTbCollectionPedidoTb = em.merge(oldClienteIdOfPedidoTbCollectionPedidoTb);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(ClienteTb clienteTb) throws IllegalOrphanException, NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            ClienteTb persistentClienteTb = em.find(ClienteTb.class, clienteTb.getClienteId());
+            Collection<PedidoTb> pedidoTbCollectionOld = persistentClienteTb.getPedidoTbCollection();
+            Collection<PedidoTb> pedidoTbCollectionNew = clienteTb.getPedidoTbCollection();
+            List<String> illegalOrphanMessages = null;
+            for (PedidoTb pedidoTbCollectionOldPedidoTb : pedidoTbCollectionOld) {
+                if (!pedidoTbCollectionNew.contains(pedidoTbCollectionOldPedidoTb)) {
+                    if (illegalOrphanMessages == null) {
+                        illegalOrphanMessages = new ArrayList<String>();
+                    }
+                    illegalOrphanMessages.add("You must retain PedidoTb " + pedidoTbCollectionOldPedidoTb + " since its clienteId field is not nullable.");
+                }
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            Collection<PedidoTb> attachedPedidoTbCollectionNew = new ArrayList<PedidoTb>();
+            for (PedidoTb pedidoTbCollectionNewPedidoTbToAttach : pedidoTbCollectionNew) {
+                pedidoTbCollectionNewPedidoTbToAttach = em.getReference(pedidoTbCollectionNewPedidoTbToAttach.getClass(), pedidoTbCollectionNewPedidoTbToAttach.getPedidoId());
+                attachedPedidoTbCollectionNew.add(pedidoTbCollectionNewPedidoTbToAttach);
+            }
+            pedidoTbCollectionNew = attachedPedidoTbCollectionNew;
+            clienteTb.setPedidoTbCollection(pedidoTbCollectionNew);
+            clienteTb = em.merge(clienteTb);
+            for (PedidoTb pedidoTbCollectionNewPedidoTb : pedidoTbCollectionNew) {
+                if (!pedidoTbCollectionOld.contains(pedidoTbCollectionNewPedidoTb)) {
+                    ClienteTb oldClienteIdOfPedidoTbCollectionNewPedidoTb = pedidoTbCollectionNewPedidoTb.getClienteId();
+                    pedidoTbCollectionNewPedidoTb.setClienteId(clienteTb);
+                    pedidoTbCollectionNewPedidoTb = em.merge(pedidoTbCollectionNewPedidoTb);
+                    if (oldClienteIdOfPedidoTbCollectionNewPedidoTb != null && !oldClienteIdOfPedidoTbCollectionNewPedidoTb.equals(clienteTb)) {
+                        oldClienteIdOfPedidoTbCollectionNewPedidoTb.getPedidoTbCollection().remove(pedidoTbCollectionNewPedidoTb);
+                        oldClienteIdOfPedidoTbCollectionNewPedidoTb = em.merge(oldClienteIdOfPedidoTbCollectionNewPedidoTb);
+                    }
+                }
+            }
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = clienteTb.getClienteId();
+                if (findClienteTb(id) == null) {
+                    throw new NonexistentEntityException("The clienteTb with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws IllegalOrphanException, NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            ClienteTb clienteTb;
+            try {
+                clienteTb = em.getReference(ClienteTb.class, id);
+                clienteTb.getClienteId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The clienteTb with id " + id + " no longer exists.", enfe);
+            }
+            List<String> illegalOrphanMessages = null;
+            Collection<PedidoTb> pedidoTbCollectionOrphanCheck = clienteTb.getPedidoTbCollection();
+            for (PedidoTb pedidoTbCollectionOrphanCheckPedidoTb : pedidoTbCollectionOrphanCheck) {
+                if (illegalOrphanMessages == null) {
+                    illegalOrphanMessages = new ArrayList<String>();
+                }
+                illegalOrphanMessages.add("This ClienteTb (" + clienteTb + ") cannot be destroyed since the PedidoTb " + pedidoTbCollectionOrphanCheckPedidoTb + " in its pedidoTbCollection field has a non-nullable clienteId field.");
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            em.remove(clienteTb);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<ClienteTb> findClienteTbEntities() {
+        return findClienteTbEntities(true, -1, -1);
+    }
+
+    public List<ClienteTb> findClienteTbEntities(int maxResults, int firstResult) {
+        return findClienteTbEntities(false, maxResults, firstResult);
+    }
+
+    private List<ClienteTb> findClienteTbEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(ClienteTb.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public ClienteTb findClienteTb(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(ClienteTb.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getClienteTbCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<ClienteTb> rt = cq.from(ClienteTb.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/main/java/com/mycompany/pizzaproject/models/ClienteTb.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pizzaproject.models;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Michael
+ */
+@Entity
+@Table(name = "cliente_tb")
+@NamedQueries({
+    @NamedQuery(name = "ClienteTb.findAll", query = "SELECT c FROM ClienteTb c"),
+    @NamedQuery(name = "ClienteTb.findByClienteId", query = "SELECT c FROM ClienteTb c WHERE c.clienteId = :clienteId"),
+    @NamedQuery(name = "ClienteTb.findByNome", query = "SELECT c FROM ClienteTb c WHERE c.nome = :nome"),
+    @NamedQuery(name = "ClienteTb.findBySobrenome", query = "SELECT c FROM ClienteTb c WHERE c.sobrenome = :sobrenome"),
+    @NamedQuery(name = "ClienteTb.findByTelefone", query = "SELECT c FROM ClienteTb c WHERE c.telefone = :telefone")})
+public class ClienteTb implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "cliente_id")
+    private Integer clienteId;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "sobrenome")
+    private String sobrenome;
+    @Basic(optional = false)
+    @Column(name = "telefone")
+    private String telefone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clienteId")
+    private Collection<PedidoTb> pedidoTbCollection;
+
+    public ClienteTb() {
+    }
+
+    public ClienteTb(Integer clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public ClienteTb(Integer clienteId, String nome, String telefone) {
+        this.clienteId = clienteId;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Integer getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Integer clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Collection<PedidoTb> getPedidoTbCollection() {
+        return pedidoTbCollection;
+    }
+
+    public void setPedidoTbCollection(Collection<PedidoTb> pedidoTbCollection) {
+        this.pedidoTbCollection = pedidoTbCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (clienteId != null ? clienteId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ClienteTb)) {
+            return false;
+        }
+        ClienteTb other = (ClienteTb) object;
+        if ((this.clienteId == null && other.clienteId != null) || (this.clienteId != null && !this.clienteId.equals(other.clienteId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.pizzaproject.models.ClienteTb[ clienteId=" + clienteId + " ]";
+    }
+    
+}
